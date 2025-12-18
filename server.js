9dce18a36fb9c97cf58fccfd65d44949375ee44f
varunVerse-creater <varunvermadev0@gmail.com>
2025-12-18 10:43:32 +0530

    increase the json capasity in server.js file

--- server.js
+++ server.js
@@ -3,32 +3,25 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
-
 const analyzeRoutes = require("./routes/analyzeRoutes");
 const authRoutes = require("./routes/authRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 
-
 const app = express();
 
-
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 connectDB();
-
 
 app.use("/api/analyze", analyzeRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/payment", paymentRoutes);
 
-
 app.get("/", (req, res) => {
-res.send("LinkedIn Analyzer API Running");
+  res.send("LinkedIn Analyzer API Running");
 });
-
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
